e2512117c142d84d252dd4b5a78102f0abbbd1eb
Luis D Munoz <ysldaves@gmail.com>
2026-07-10 11:18:50 -0600

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> stack;   // vector as stack, so we can sum at the end
+
+        for (const string& op : operations) {
+            if (op == "+") {
+                int n = stack.size();
+                stack.push_back(stack[n - 1] + stack[n - 2]);
+            } else if (op == "D") {
+                stack.push_back(2 * stack.back());
+            } else if (op == "C") {
+                stack.pop_back();
+            } else {
+                stack.push_back(stoi(op));   // convert string to int
+            }
+        }
+
+        return accumulate(stack.begin(), stack.end(), 0);
+    }
+};
